c9820b9e3cb11324ed339881c06985fa84c590cb
Mati Horowitz <mati@elementor.com>
2023-11-07 11:32:11 +0200

    Attachments

--- src/main.jsx
+++ src/main.jsx
@@ -1,7 +1,13 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
-import {elementorCommon} from "./elementorCommon.js";
+import { elementorCommon } from './elementorCommon.js';
+
 window.elementorCommon = elementorCommon;
+
+window.ElementorAiConfig = {
+	url_to_container_url: 'http://localhost:3000',
+};
+
 import LayoutApp from './../../elementor-editor-env/plugins/elementor/modules/ai/assets/js/editor/layout-app.js';
 
 ReactDOM.createRoot( document.getElementById( 'root' ) ).render(
@@ -12,6 +18,29 @@ ReactDOM.createRoot( document.getElementById( 'root' ) ).render(
 			position: 'absolute',
 		}}>
 			<LayoutApp
+				attachmentsTypes={{
+					json: {
+						promptSuggestions: [
+							{ text: '[JSON] Create a layout for a blog post' },
+							{ text: '[JSON] Create a layout for a product page' },
+							{ text: '[JSON] Create a layout for a contact page' },
+						],
+					},
+					url: {
+						promptSuggestions: [
+							{ text: '[URL] Create a layout for a blog post' },
+							{ text: '[URL] Create a layout for a product page' },
+							{ text: '[URL] Create a layout for a contact page' },
+						],
+
+					},
+				}}
+				attachments={[{
+						type: 'url',
+						previewHTML: ``,
+						content: '{}',
+						label: 'domain.com',
+					}]}
 				onData={() => {
 					console.log( 'onData' );
 				}}

6cdc2d2b209e567c3ed7c5f72ba4a4141961d9f0
Mati Horowitz <mati@elementor.com>
2023-12-20 14:05:20 +0200

    Fix theme provider

--- src/main.jsx
+++ src/main.jsx
@@ -6,6 +6,7 @@ import { JSON1, SCREENSHOT_DARK_1, SCREENSHOT_LIGHT_1, URL1, URL2 } from './mock
 window.elementorCommon = elementorCommon;
 
 import LayoutApp from './../../elementor-editor-env/plugins/elementor/modules/ai/assets/js/editor/layout-app.js';
+import { DirectionProvider, ThemeProvider } from '@elementor/ui';
 
 ReactDOM.createRoot( document.getElementById( 'root' ) ).render(
 	<React.Fragment>
@@ -14,55 +15,58 @@ ReactDOM.createRoot( document.getElementById( 'root' ) ).render(
 			padding: '50px',
 			position: 'absolute',
 		}}>
-			<LayoutApp
-				mode="layout"
-				attachmentsTypes={{
-					json: {
-						promptSuggestions: [
-							{ text: '[JSON] Create a layout for a blog post' },
-							{ text: '[JSON] Create a layout for a product page' },
-							{ text: '[JSON] Create a layout for a contact page' },
-						],
-					},
-					url: {
-						promptSuggestions: [
-							{ text: '[URL] Create a layout for a blog post' },
-							{ text: '[URL] Create a layout for a product page' },
-							{ text: '[URL] Create a layout for a contact page' },
-						],
-					},
-				}}
-				attachments={[
-					// { ...URL1 },
-					// { ...URL2 },
-					// { ...JSON1 },
-				]}
-				onData={() => {
-					console.log( 'onData' );
-					// return SCREENSHOT_LIGHT_1;
-					// return SCREENSHOT_DARK_1;
-				}}
-				onConnect={() => {
-					console.log( 'onConnect' );
-				}}
-				onDisconnect={() => {
-					console.log( 'onDisconnect' );
-				}}
-				onGenerate={() => {
-					console.log( 'onGenerate' );
-				}}
-				onInsert={() => {
-					console.log( 'onInsert' );
-				}}
-				onSelect={() => {
-					console.log( 'onSelect' );
-				}}
-				colorScheme="auto"
-				onClose={() => {
-					console.log( 'onClose' );
-				}}
-				isRTL={false}
-			/>
+			<DirectionProvider rtl={ false }>
+				<ThemeProvider colorScheme="auto">
+					<LayoutApp
+						mode="layout"
+						editorSessionId={`editor-session-${Math.random()}`}
+						attachmentsTypes={{
+							json: {
+								promptSuggestions: [
+									{ text: '[JSON] Create a layout for a blog post' },
+									{ text: '[JSON] Create a layout for a product page' },
+									{ text: '[JSON] Create a layout for a contact page' },
+								],
+							},
+							url: {
+								promptSuggestions: [
+									{ text: '[URL] Create a layout for a blog post' },
+									{ text: '[URL] Create a layout for a product page' },
+									{ text: '[URL] Create a layout for a contact page' },
+								],
+							},
+						}}
+						attachments={[
+							// { ...URL1 },
+							// { ...URL2 },
+							// { ...JSON1 },
+						]}
+						onData={() => {
+							console.log( 'onData' );
+							return SCREENSHOT_LIGHT_1;
+							// return SCREENSHOT_DARK_1;
+						}}
+						onConnect={() => {
+							console.log( 'onConnect' );
+						}}
+						onDisconnect={() => {
+							console.log( 'onDisconnect' );
+						}}
+						onGenerate={() => {
+							console.log( 'onGenerate' );
+						}}
+						onInsert={() => {
+							console.log( 'onInsert' );
+						}}
+						onSelect={() => {
+							console.log( 'onSelect' );
+						}}
+						onClose={() => {
+							console.log( 'onClose' );
+						}}
+					/>
+				</ThemeProvider>
+			</DirectionProvider>
 		</div>
 	</React.Fragment>,
 );
